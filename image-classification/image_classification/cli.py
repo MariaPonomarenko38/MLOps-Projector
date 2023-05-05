e94530388057ea4dce6b83245cec9df7aa6e4148
MariaPonomarenko38 <mashaponomarenko2002@gmail.com>
2023-05-05 18:01:26 +0200

    deleted some files

--- image-classification/image_classification/cli.py
+++ image-classification/image_classification/cli.py
@@ -6,15 +6,15 @@ from utils import initialize_wandb
 app = typer.Typer()
 
 @app.command()
-def getdata(path_to_args):
+def getData(path_to_args):
     get_data(path_to_args)
 
 @app.command()
-def processdata(path_to_args):
+def processData(path_to_args):
     process_data(path_to_args)
 
 @app.command()
-def traini(path_to_args):
+def trainModel(path_to_args):
     train(path_to_args)
 
 if __name__ == "__main__":
